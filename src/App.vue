5ef42ed5f51248d55e8f6da32d158debd1232d1c
markusand <mvilella@obsa.ad>
2020-11-06 18:40:21 +0100

    Add Candidacy view

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
 	<div class="wrapper">
+		<candidacy />
 		<faq />
 	</div>
 </template>
 
 <script>
+import Candidacy from './views/Candidacy.vue';
 import Faq from './views/FAQ.vue';
 
 export default {
 	name: 'App',
 	components: {
+		Candidacy,
 		Faq,
 	},
 };
---
+++ src/views/Candidacy.vue
@@ -0,0 +1,16 @@
+<template>
+	<section class="split candidacy">
+		<header>
+			<h2>{{ $t('candidacy.title') }}</h2>
+		</header>
+		<div class="content">
+			<p>{{ $t('candidacy.description') }}</p>
+		</div>
+	</section>
+</template>
+
+<script>
+export default {
+	name: 'Candidacy',
+};
+</script>
