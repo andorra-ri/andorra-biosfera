9cc045c238a34c13c685aff73b1fb034456cb243
markusand <mvilella@obsa.ad>
2020-11-06 13:54:42 +0100

    Add FAQ

---
+++ src/views/FAQ.vue
@@ -0,0 +1,24 @@
+<template>
+	<section class="split faq">
+		<header>
+			<h2>{{ $t('faq.title') }}</h2>
+		</header>
+		<div class="content">
+			<details v-for="question in questions" :key="question.question" open>
+				<summary>{{ question.question }}</summary>
+				<p>{{ question.answer }}</p>
+			</details>
+		</div>
+	</section>
+</template>
+
+<script>
+export default {
+	name: 'FAQ',
+	computed: {
+		questions() {
+			return this.$t('faq.questions');
+		},
+	},
+};
+</script>
