6d28c2e40df69fabf7999da140bc2acb59b32de6
markusand <mvilella@obsa.ad>
2020-11-06 15:07:45 +0100

    Add FAQ search

--- src/views/FAQ.vue
+++ src/views/FAQ.vue
@@ -1,5 +1,12 @@
 <template>
 	<section class="split faq">
+		<aside>
+			<input
+				v-model="search"
+				:placeholder="$t('faq.search')"
+				type="text"
+				class="search">
+		</aside>
 		<header>
 			<h2>{{ $t('faq.title') }}</h2>
 			<div class="contact">
@@ -25,15 +32,27 @@
 import NavSocial from '../components/NavSocial.vue';
 import { mails } from '../config.yaml';
 
+const normalize = str => str.normalize('NFD').replace(/[\u0300-\u036f]/g, '').toLowerCase();
+const find = (str, texts) => texts.reduce((acc, txt) => acc || normalize(txt).includes(str), false);
+
 export default {
 	name: 'FAQ',
 	components: { NavSocial },
 	data() {
-		return { email: mails.inquiry };
+		return {
+			search: '',
+			group: undefined,
+			email: mails.inquiry,
+		};
 	},
 	computed: {
 		questions() {
-			return this.$t('faq.questions');
+			const search = this.search && normalize(this.search);
+			return this.$t('faq.questions').filter(({ group, question, answer }) => {
+				const inGroup = this.group ? group === this.group : true;
+				const isFound = search ? find(search, [question, answer]) : true;
+				return inGroup && isFound;
+			});
 		},
 	},
 };
