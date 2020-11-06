6ff40d6e63279e5eaf9c3b3481e6f8b310f01995
markusand <mvilella@obsa.ad>
2020-11-06 12:32:05 +0100

    Init repo

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import Vue from 'vue';
+import App from './App.vue';
+
+import 'reset-css';
+import './styles/index.scss';
+
+/* eslint-disable no-new */
+new Vue({
+	el: '#app',
+	render: h => h(App),
+});
