1a49a6efa34c894d9f747ff23517dcb50cbad6a8
markusand <mvilella@obsa.ad>
2020-11-06 18:44:08 +0100

    Add candidacy timeline

--- src/views/Candidacy.vue
+++ src/views/Candidacy.vue
@@ -5,12 +5,33 @@
 		</header>
 		<div class="content">
 			<p>{{ $t('candidacy.description') }}</p>
+			<ul class="timeline">
+				<li
+					v-for="(begin, name) in steps"
+					:key="name"
+					:class="{ 'is-started': begin < new Date() }">
+					<em>{{ begin | date | capitalize }}</em>
+					<h4>{{ $t(`candidacy.steps.${name}.name`) }}</h4>
+					<p>{{ $t(`candidacy.steps.${name}.description`) }}</p>
+				</li>
+			</ul>
 		</div>
 	</section>
 </template>
 
 <script>
+import { candidacy } from '../config.yaml';
+
+const { steps } = candidacy;
+
 export default {
 	name: 'Candidacy',
+	filters: {
+		date: epoch => new Date(epoch).toLocaleString('ca', { month: 'long', year: 'numeric' }),
+		capitalize: text => text.charAt(0).toUpperCase() + text.slice(1),
+	},
+	data() {
+		return { steps };
+	},
 };
 </script>
