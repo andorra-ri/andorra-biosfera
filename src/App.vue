1b71da41faa395083f1a840d1ec3f477d87874be
markusand <mvilella@obsa.ad>
2020-11-07 13:48:28 +0100

    Add home landing section

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
 	<div class="wrapper">
-		<candidacy />
-		<faq />
+		<home />
+		<candidacy id="candidatura" />
+		<faq id="faqs" />
 	</div>
 </template>
 
 <script>
+import Home from './views/Home.vue';
 import Candidacy from './views/Candidacy.vue';
 import Faq from './views/FAQ.vue';
 
 export default {
 	name: 'App',
 	components: {
+		Home,
 		Candidacy,
 		Faq,
 	},
---
+++ src/views/Home.vue
@@ -0,0 +1,30 @@
+<template>
+	<section class="home">
+		<div class="landing">
+			<nav class="topbar">
+				<div class="topbar__right">
+					<ul class="main-nav">
+						<li v-for="(section, name) in nav" :key="name">
+							<a :href="`#${section}`">{{ $t(`nav.${name}`) }}</a>
+						</li>
+					</ul>
+					<a :href="join" target="_blank" class="cta">{{ $t('nav.join') }}</a>
+				</div>
+			</nav>
+		</div>
+	</section>
+</template>
+
+<script>
+import { nav, join } from '../config.yaml';
+
+export default {
+	name: 'Home',
+	data() {
+		return {
+			nav,
+			join,
+		};
+	},
+};
+</script>
